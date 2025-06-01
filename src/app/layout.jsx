ce880c13085055275f10a56a0e0ca4fd25032067
BLI <zyclone271@outlook.com>
2025-06-01 17:23:01 -0500

    Analytics integration. Project Upgrades.
    Specifics:
    - integrated Google Analytics and Google Tag Manager
    - return to project to fix responsiveness
    - missing components to be optimized and added later

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,9 @@ import "@/styles/global.css";
 import Navbar from "@/components/shared-components/navbar-components/Navbar";
 import Footer from "@/components/shared-components/footer-components/Footer";
 import JumpToTopButton from "@/components/shared-components/JumpToTopButton";
+import Script from 'next/script';
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata = {
   title: "Audiophile – Hear the Difference | Frontend Mentor Project | Bryan Li",
@@ -15,7 +18,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        {/* Google Tag Manager Script (Head) */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body className="relative h-full min-h-screen font-theme-manrope overflow-x-hidden overflow-y-auto !m-0 !p-0 example">
         <Navbar />
         <main className="w-full h-full min-h-screen !m-0 !p-0">
@@ -23,6 +41,16 @@ export default function RootLayout({ children }) {
         </main>
         <JumpToTopButton />
         <Footer />
+
+        {/* Google Tag Manager noscript (for non-JS support) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
